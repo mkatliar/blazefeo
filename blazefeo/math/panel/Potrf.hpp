16cf3d7c9d98c097587df712d0680cdc79f71c35
Mikhail Katliar <cepstr@gmail.com>
2019-11-04 22:50:28 +0300

    Performance optimization: main loop pseudo-unrolling in potrf

--- blazefeo/math/panel/Potrf.hpp
+++ blazefeo/math/panel/Potrf.hpp
@@ -17,6 +17,33 @@ namespace blazefeo
 
 
     template <typename MT1, typename MT2>
+    BLAZE_ALWAYS_INLINE void potrf_backend(size_t k,
+        PanelMatrix<MT1, rowMajor> const& A, PanelMatrix<MT2, rowMajor>& L)
+    {
+        using ET = ElementType_t<MT1>;
+        size_t constexpr TILE_SIZE = TileSize_v<ET>;
+            
+        size_t const M = rows(A);
+        size_t const K = k * TILE_SIZE;
+
+        auto L1 = submatrix(~L, K, K, M - K, TILE_SIZE, unchecked);
+        gemm_nt(ET(-1.), ET(1.),
+            submatrix(~L, K, 0, M - K, K, unchecked),
+            submatrix(~L, K, 0, TILE_SIZE, K, unchecked),
+            submatrix(~A, K, K, M - K, TILE_SIZE, unchecked),
+            L1);
+
+        RegisterMatrix<ET, 1, 1, TILE_SIZE> ker;
+        load(ker, tile(L, k, k), spacing(L));
+        ker.potrf();
+        store(ker, tile(L, k, k), spacing(L));
+
+        for (size_t i = k + 1; (i + 1) * TILE_SIZE <= M; ++i)
+            trsm<false, false, true>(ker, tile(L, i, k), tile(L, i, k));
+    }
+
+
+    template <typename MT1, typename MT2>
     BLAZE_ALWAYS_INLINE void potrf(
         PanelMatrix<MT1, rowMajor> const& A, PanelMatrix<MT2, rowMajor>& L)
     {
@@ -38,6 +65,8 @@ namespace blazefeo
 
         RegisterMatrix<ET, 1, 1, TILE_SIZE> ker;
 
+        size_t k = 0;
+
         if (TILE_SIZE <= M)
         {
             // Zero-out upper blocks
@@ -50,37 +79,24 @@ namespace blazefeo
             for (size_t i = 1; (i + 1) * TILE_SIZE <= M; ++i)
                 trsm<false, false, true>(ker, tile(A, i, 0), tile(L, i, 0));
         }
-            
-        // #pragma unroll
-        for (size_t k = 1; (k + 1) * TILE_SIZE <= M; ++k)
-        {
-            // Zero-out upper blocks
-            // std::fill_n(tile(L, k, k + 1), TILE_SIZE * (M - TILE_SIZE * (k + 1)), ET {});
 
-            // for (size_t i = 0; i < k; ++i)
-            //     std::fill_n(tile(L, i, k), TILE_SIZE * TILE_SIZE, ET {});
 
-            size_t const K = k * TILE_SIZE;
-            auto L1 = submatrix(~L, K, K, M - K, TILE_SIZE, unchecked);
-            gemm_nt(ET(-1.), ET(1.),
-                submatrix(~L, K, 0, M - K, K, unchecked),
-                submatrix(~L, K, 0, TILE_SIZE, K, unchecked),
-                submatrix(~A, K, K, M - K, TILE_SIZE, unchecked),
-                L1);
+        if (TILE_SIZE * (++k + 1) <= M)
+            potrf_backend(k, A, L); // k = 1
 
-            // for (; (i + 1) * TILE_SIZE <= M; ++i)
-            // {   
-            //     gemm_backend<false, true>(ker, K, -1., 1.,
-            //         tile(L, i, 0), spacing(L), tile(L, k, 0), spacing(L),
-            //         tile(A, i, k), spacing(A), tile(L, i, k), spacing(L));
-            // }
+        if (TILE_SIZE * (++k + 1) <= M)
+            potrf_backend(k, A, L); // k = 2
 
-            load(ker, tile(L, k, k), spacing(L));
-            ker.potrf();
-            store(ker, tile(L, k, k), spacing(L));
+        if (TILE_SIZE * (++k + 1) <= M)
+            potrf_backend(k, A, L); // k = 3
 
-            for (size_t i = k + 1; (i + 1) * TILE_SIZE <= M; ++i)
-                trsm<false, false, true>(ker, tile(L, i, k), tile(L, i, k));
-        }
+        if (TILE_SIZE * (++k + 1) <= M)
+            potrf_backend(k, A, L); // k = 4
+
+        if (TILE_SIZE * (++k + 1) <= M)
+            potrf_backend(k, A, L); // k = 5
+        
+        while (TILE_SIZE * (++k + 1) <= M) 
+            potrf_backend(k, A, L); // k = 6,7,...
     }
 }
