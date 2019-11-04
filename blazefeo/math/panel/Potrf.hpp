67ff3934ce8db4195e92955ae13625bd438e5a8b
Mikhail Katliar <cepstr@gmail.com>
2019-11-04 18:08:24 +0300

    Fixed bug in potrf().

--- blazefeo/math/panel/Potrf.hpp
+++ blazefeo/math/panel/Potrf.hpp
@@ -38,16 +38,28 @@ namespace blazefeo
 
         GemmKernel<ET, 1, 1, TILE_SIZE> ker;
 
-        for (size_t k = 0; (k + 1) * TILE_SIZE <= M; ++k)
+        // if (M == 4)
+        // {
+        //     load(ker, tile(A, 0, 0), spacing(A));
+        //     ker.potrf();
+        //     store(ker, tile(L, 0, 0), spacing(L));
+        // }
+        // else if (M == 8)
+        // {
+        //     load(ker, tile(A, 0, 0), spacing(A));
+        //     ker.potrf();
+        //     store(ker, tile(L, 0, 0), spacing(L));
+        // }
+        // else
         {
-            // Zero-out upper blocks
-            std::fill_n(tile(L, k, k + 1), TILE_SIZE * (M - TILE_SIZE * (k + 1)), ET {});
-
-            // for (size_t i = 0; i < k; ++i)
-            //     std::fill_n(tile(L, i, k), TILE_SIZE * TILE_SIZE, ET {});
-
-            if (k > 0)
+            for (size_t k = 0; (k + 1) * TILE_SIZE <= M; ++k)
             {
+                // Zero-out upper blocks
+                std::fill_n(tile(L, k, k + 1), TILE_SIZE * (M - TILE_SIZE * (k + 1)), ET {});
+
+                // for (size_t i = 0; i < k; ++i)
+                //     std::fill_n(tile(L, i, k), TILE_SIZE * TILE_SIZE, ET {});
+
                 size_t const K = k * TILE_SIZE;
                 auto L1 = submatrix(~L, K, K, M - K, TILE_SIZE, unchecked);
                 gemm_nt(ET(-1.), ET(1.),
@@ -55,21 +67,21 @@ namespace blazefeo
                     submatrix(~L, K, 0, TILE_SIZE, K, unchecked),
                     submatrix(~A, K, K, M - K, TILE_SIZE, unchecked),
                     L1);
+
+                // for (; (i + 1) * TILE_SIZE <= M; ++i)
+                // {   
+                //     gemm_backend<false, true>(ker, K, -1., 1.,
+                //         tile(L, i, 0), spacing(L), tile(L, k, 0), spacing(L),
+                //         tile(A, i, k), spacing(A), tile(L, i, k), spacing(L));
+                // }
+
+                load(ker, tile(L, k, k), spacing(L));
+                ker.potrf();
+                store(ker, tile(L, k, k), spacing(L));
+
+                for (size_t i = k + 1; (i + 1) * TILE_SIZE <= M; ++i)
+                    trsm<false, false, true>(ker, tile(L, i, k), tile(L, i, k));
             }
-
-            // for (; (i + 1) * TILE_SIZE <= M; ++i)
-            // {   
-            //     gemm_backend<false, true>(ker, K, -1., 1.,
-            //         tile(L, i, 0), spacing(L), tile(L, k, 0), spacing(L),
-            //         tile(A, i, k), spacing(A), tile(L, i, k), spacing(L));
-            // }
-
-            load(ker, tile(L, k, k), spacing(L));
-            ker.potrf();
-            store(ker, tile(L, k, k), spacing(L));
-
-            for (size_t i = k + 1; (i + 1) * TILE_SIZE <= M; ++i)
-                trsm<false, false, true>(ker, tile(L, i, k), tile(L, i, k));
         }
     }
 }
